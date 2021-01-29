6c436e47dc284ece7550c8395287d9a523c73357
Nathan <nathan.constantine-cooke@ed.ac.uk>
2021-01-29 12:39:09 +0000

    Add fix_dates()
    Cleans columns of a dataframe consisting of messy inconsistent date data (such as date data which was entered via free-text boxes).

--- R/package.R
+++ R/package.R
@@ -1,12 +1,14 @@
 #' rvallejos: The group R package for the Vallejos group.
 #'
 #' This package contains a lot of TODO!
-#' 
+#'
 #' @section Enrichment analysis:
 #' GOstats - use this.
 #'
-#' 
+#' @section Utility:
+#' \link{fix_dates} - fix messy inconsistent dates entered via a free-text box
+#'
 #' @docType package
-#' 
+#'
 #' @name rvallejos
 NULL
